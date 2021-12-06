9af06a0e9f330c2889b794fa8f087e41b2df7225
yassirf <yf286@cam.ac.uk>
2021-12-06 14:47:06 +0000

    Update decoder.py

--- examples/self_distribution_distillation/self_distribution_distillation_src/modules/decoder.py
+++ examples/self_distribution_distillation/self_distribution_distillation_src/modules/decoder.py
@@ -369,8 +369,8 @@ class MimoTransformerDecoder(TransformerDecoder):
                 self.embed_tokens.weight.shape[0] * cfg.num_heads,
                 bias=cfg.bias,
             )
-            weight = torch.cat([self.embed_tokens.weight for _ in range(cfg.num_heads)])
-            self.output_projection.weight = torch.nn.Parameter(weight)
+            # weight = torch.cat([self.embed_tokens.weight for _ in range(cfg.num_heads)])
+            # self.output_projection.weight = torch.nn.Parameter(weight)
         else:
             self.output_projection = nn.Linear(
                 self.output_embed_dim, len(dictionary) * cfg.num_heads, bias=cfg.bias
