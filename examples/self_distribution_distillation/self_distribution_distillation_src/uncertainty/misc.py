ce81259de387456fc3cfb8806c92e1ab65bbf0d4
yassirf <yf286@cam.ac.uk>
2021-12-30 11:57:48 +0100

    Update misc.py

--- examples/self_distribution_distillation/self_distribution_distillation_src/uncertainty/misc.py
+++ examples/self_distribution_distillation/self_distribution_distillation_src/uncertainty/misc.py
@@ -26,12 +26,12 @@ def process_outputs_gaussian(outputs, extra: List[Dict] = None):
     """
     If extra has 'teacher_predictions_lp' then access these.
     """
-    if extra is None: return outputs
+    if extra is None: return outputs, extra
 
     check = 'teacher_predictions_lp' in extra[0]
 
     # Returns a list of predictions
-    if not check: return outputs
+    if not check: return outputs, extra
 
     # If these do exist then get the number of predictions in each member
     num = extra[0]['teacher_predictions_lp'].size(1)
@@ -43,9 +43,9 @@ def process_outputs_gaussian(outputs, extra: List[Dict] = None):
         for i in range(num)
     ]
 
-    # The scale of gaussian
+    # A list of dictionaries for scale of gaussian
     extra = [
-        ex['student_predictions_scale'][:, i]
+        {'student_predictions_scale': ex['student_predictions_scale'][:, i]}
         for ex in extra
         for i in range(num)
     ]
