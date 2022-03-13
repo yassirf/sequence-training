9f55d5fe8890316dee9a792347f4cb5d757e2b3a
yassirf <yf286@cam.ac.uk>
2022-03-13 19:57:54 +0000

    Update __init__.py

--- examples/self_distribution_distillation/self_distribution_distillation_src/usertasks/__init__.py
+++ examples/self_distribution_distillation/self_distribution_distillation_src/usertasks/__init__.py
@@ -1,9 +1,12 @@
 
 from .translation import TranslationUncertaintyTask
+from .surrogate import TranslationSurrogateUncertaintyTask
 from .distillation import DistillationTask, DistillationAndGaussTask
+
 
 __all__ = [
     'TranslationUncertaintyTask',
+    'TranslationSurrogateUncertaintyTask',
     'DistillationTask',
     'DistillationAndGaussTask',
 ]
